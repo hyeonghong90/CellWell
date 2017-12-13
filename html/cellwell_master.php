b5d1f1f5b7612291d1cd3d464b8bb8be8c52af9d
Jan Santos <jsantos6@terpmail.umd.edu>
2017-12-13 13:12:44 -0500

    results now injected to phone container

--- html/cellwell_master.php
+++ html/cellwell_master.php
@@ -20,7 +20,7 @@ function query_to_db($conn, $sql){
     if ($result) {   
     	if (mysqli_num_rows($result) > 0){
             while($row = mysqli_fetch_assoc($result)) {
-                echo $row["phoneMaker"] . " " . $row["cellName"] . "<br>";
+                echo "<div class='phone'><h3>" . $row["cellName"] . "</h3></div>";
             }
     		// echo "Your query was successful";
     	} else {
@@ -31,14 +31,14 @@ function query_to_db($conn, $sql){
     }
 }
 
-// Creating a query
+/*// Creating a query
 $query = "SELECT * FROM celldata as c1
 JOIN celldata_has_cellbands c2 ON c1.cellID = c2.cellData_cellID
 JOIN cellbands as c3 ON c2.cellBands_cellBandID = c3.cellBandID
 JOIN cellcarriers_has_cellbands c4 ON c3.cellBandID = c4.cellBands_cellBandID
-JOIN cellcarriers as c5 ON c4.cellCarriers_carrierID = c5.carrierID\n";
+JOIN cellcarriers as c5 ON c4.cellCarriers_carrierID = c5.carrierID\n";*/
 
-// $query = "SELECT * FROM celldata as c1\n";
+$query = "SELECT * FROM celldata as c1\n";
 
 // Adding conditions based on the user query
 if (strpos($query, 'WHERE') == false){
