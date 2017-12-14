d68d7ef6dcfbdaccdc136db497c3b26a3d55ca09
Hyeong-gi <hyeonghong90@gmail.com>
2017-12-14 17:42:05 -0500

    e

--- html/cellwell_master.php
+++ html/cellwell_master.php
@@ -39,9 +39,8 @@ $query = "SELECT * FROM celldata as c1
 JOIN celldata_has_cellbands c2 ON c1.cellID = c2.cellData_cellID
 JOIN cellbands as c3 ON c2.cellBands_cellBandID = c3.cellBandID
 JOIN cellcarriers_has_cellbands c4 ON c3.cellBandID = c4.cellBands_cellBandID
-JOIN cellcarriers as c5 ON c4.cellCarriers_carrierID = c5.carrierID\n";
-
-$query = "SELECT * FROM celldata as c1\n";
+JOIN cellcarriers as c5 ON c4.cellCarriers_carrierID = c5.carrierID
+JOIN pictures as pic ON c1.cellID = pic.cellData_cellID";
 
 // Adding conditions based on the user query
 if (strpos($query, 'WHERE') == false){
