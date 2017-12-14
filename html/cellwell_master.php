956d37b01ccb0b0fa7d1d734f885df8096561d61
Hyeong-gi <hyeonghong90@gmail.com>
2017-12-14 15:00:26 -0500

    daily update

--- html/cellwell_master.php
+++ html/cellwell_master.php
@@ -31,13 +31,13 @@ function query_to_db($conn, $sql){
     }
 }
 
-
 // Creating a query
 $query = "SELECT * FROM celldata as c1
 JOIN celldata_has_cellbands c2 ON c1.cellID = c2.cellData_cellID
 JOIN cellbands as c3 ON c2.cellBands_cellBandID = c3.cellBandID
 JOIN cellcarriers_has_cellbands c4 ON c3.cellBandID = c4.cellBands_cellBandID
-JOIN cellcarriers as c5 ON c4.cellCarriers_carrierID = c5.carrierID\n";
+JOIN cellcarriers as c5 ON c4.cellCarriers_carrierID = c5.carrierID
+JOIN pictures as pic ON c1.cellID = pic.cellData_cellID\n";
 
 $query = "SELECT * FROM celldata as c1\n";
 
